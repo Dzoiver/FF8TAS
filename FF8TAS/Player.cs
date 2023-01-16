21e8c9ba6ede37ab205e31c1ccae602237dc50a0
Bezrukov Kirill <doomseifer@mail.ru>
2023-01-16 14:08:33 +0400

    Add project files.

---
+++ FF8TAS/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FF8TAS
+{
+    class Player
+    {
+        public Process ff8Process;
+        [DllImport("User32.dll")]
+
+        static extern int SetForegroundWindow(IntPtr point);
+        public Process FindProcess()
+        {
+            Process[] ps = Process.GetProcessesByName("FF8_EN");
+            Console.WriteLine(ps.Length);
+
+            ff8Process = ps.FirstOrDefault();
+            return ff8Process;
+        }
+
+        public void MemoryInit()
+        {
+
+        }
+
+        public void SetFocus(Process process = null)
+        {
+            if (process == null)
+                process = ff8Process;
+
+            IntPtr h = process.MainWindowHandle;
+            SetForegroundWindow(h);
+        }
+
+        public void StartRun()
+        {
+            SendKeys.SendWait("{k}");
+        }
+
+
+    }
+}
---
+++ FF8TAS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FF8TAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FF8 TAS ready to start!");
+
+            Player player = new Player();
+
+            player.FindProcess();
+            player.MemoryInit();
+            player.SetFocus();
+            player.StartRun();
+            Console.ReadKey();
+
+        }
+    }
+}
